bed14ecef9dd56ca65feba59371185a18d2c7bf9
juanmanpr <juanmanpr@gmail.com>
2019-10-01 11:58:15 +0100

    res18 exp

--- experiments/ctdet_coco_res18.sh
+++ experiments/ctdet_coco_res18.sh
@@ -1,1 +1,1 @@
-python main.py ctdet --exp_id coco_res18 --arch res_18 --batch_size 2 --master_batch 2 --lr 2.5e-4
+python main.py ctdet --exp_id coco_res18 --arch res_18 --batch_size 128  --master_batch 9 --lr 5e-4 --gpus 0,1,2,3,4,5,6,7 --num_workers 16
