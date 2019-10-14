d3d632d5deece90fde868f1193682d436df4c4a9
JuanMa <j.perez-rua@samsung.com>
2019-10-14 13:10:08 +0100

    int + 0.1 for draw_msra_gaussian; then solving bug for draw_umich_gaussian

--- src/lib/utils/image.py
+++ src/lib/utils/image.py
@@ -127,12 +127,12 @@ def draw_umich_gaussian(heatmap, center, radius, k=1):
   diameter = 2 * radius + 1
   gaussian = gaussian2D((diameter, diameter), sigma=diameter / 6)
   
-  x, y = int(center[0]), int(center[1])
+  x, y = center[0], center[1]
 
   height, width = heatmap.shape[0:2]
     
-  left, right = min(x, radius), min(width - x, radius + 1)
-  top, bottom = min(y, radius), min(height - y, radius + 1)
+  left, right = int(min(x, radius)), int(min(width - x, radius + 1))
+  top, bottom = int(min(y, radius)), int(min(height - y, radius + 1))
 
   masked_heatmap  = heatmap[y - top:y + bottom, x - left:x + right]
   masked_gaussian = gaussian[radius - top:radius + bottom, radius - left:radius + right]
