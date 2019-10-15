f13929ac45988618a9c9c26393484aa87eb1b0e7
JuanMa <j.perez-rua@samsung.com>
2019-10-15 10:39:07 +0100

    latest

--- experiments/epdet_coco_res.sh
+++ experiments/epdet_coco_res.sh
@@ -8,4 +8,5 @@ python main_ep.py epdet --exp_id coco_ep_res50 --dataset coco_ep --arch resrw_50
 
 ################# COCO FULL RES-101 (RES-18 is used for meta)
 python main.py epdet --exp_id coco_ep_res101_nohc  --dataset coco_ep --arch resrw_101 --batch_size 80 --master_batch 10 --lr 1.00e-5 --gpus 0,1,2,3,4,5,6,7 --num_workers 16 --head_conv 0 --load_basemodel '../models/coco_res101_nohc/model_best.pth' --load_metamodel '../models/coco_res18_nohc/model_best.pth' --wh_weight 0.000001  --off_weight 0.00001 --head_conv 0
+# python test_ep.py epdet --exp_id coco_ep_res101  --dataset coco_ep --arch resrw_101 --head_conv 0 --resume
 
