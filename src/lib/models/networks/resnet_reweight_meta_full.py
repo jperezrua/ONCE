a24698e6664502eaf1ec64d5fb09a76fe376ca64
juanmanpr <juanmanpr@gmail.com>
2019-10-20 16:41:05 +0100

    fast comm

--- src/lib/models/networks/resnet_reweight_meta_full.py
+++ src/lib/models/networks/resnet_reweight_meta_full.py
@@ -220,7 +220,7 @@ class PoseMSMetaResNet(nn.Module):
     def forward(self, x, y):
         B = x.size(0)
         C = x.size(1)
-        x = x.view(-1, x.shape(2), x.shape(3), x.shape(4))
+        x = x.view(-1, x.size(2), x.size(3), x.size(4))
         x = self.extract_features(x)
         print('x feature size: ',x.shape)
         ret = {}
