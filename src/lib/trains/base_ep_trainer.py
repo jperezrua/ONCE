443b0d70661d1fe8565658fd46aa92b2936213f2
juanmanpr <juanmanpr@gmail.com>
2019-10-20 20:58:35 +0100

    fast comm

--- src/lib/trains/base_ep_trainer.py
+++ src/lib/trains/base_ep_trainer.py
@@ -32,10 +32,14 @@ class ModelWithLoss(torch.nn.Module):
 
     outputs[0]['hm'] = outputs[0]['hm'].view(B*C,hmsize[2],hmsize[3],hmsize[4]).contiguous()
     outputs[0]['wh'] = outputs[0]['wh'].view(B*C,whsize[2],whsize[3],whsize[4]).contiguous()
+    outputs[0]['wh'] = torch.mean(outputs[0]['wh'], dim=1)
     outputs[0]['reg'] = outputs[0]['reg'].view(B*C,regsize[2],regsize[3],regsize[4]).contiguous()
 
     batch['hm'] = batch['hm'].view(B*C,hmsize[2],hmsize[3],hmsize[4]).contiguous()
-    
+    batch['wh'] = batch['wh'].view(B*C,-1,2).contiguous()
+    batch['ind'] = batch['ind'].view(B*C,-1).contiguous()
+
+
     loss, loss_stats = self.loss(outputs, batch)
     return outputs[-1], loss, loss_stats
 
